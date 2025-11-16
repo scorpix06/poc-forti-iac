8e7287c5d9d5bd97cd9944605b3834ba4e880238
Anon Anon <alban@MacBook-Air-de-Alban.local>
2025-11-16 09:50:29 +0100

    Update rules files names to policies

---
+++ policies.tf
@@ -0,0 +1,27 @@
+locals {
+  policies = jsondecode(file("${path.module}/data-source/fw-policies-intended.json"))
+}
+
+resource "fortios_firewall_policy" "policies" {
+  for_each = { for policy in local.policies : policy.name => policy }
+
+  depends_on = [
+    fortios_system_interface.VLANS,
+    fortios_firewall_object_address.addresses,
+    fortios_firewallservice_custom.trname
+
+  ]
+
+
+  action             = each.value.action
+  name               = each.value.name
+  schedule           = each.value.schedule
+  
+  dstaddr { name = each.value.dstaddr }
+  dstintf { name = each.value.dstintf }
+  service { name = each.value.service }
+  srcaddr { name = each.value.srcaddr }
+  srcintf { name = each.value.srcintf }
+}
+
+
